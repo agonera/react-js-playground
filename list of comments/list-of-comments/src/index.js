84b58d9d819894546e145c8280e01b2ef9b09492
Aleksandra Gonera <goneraleksandra@gmail.com>
2019-03-02 22:28:52 +0100

    added props

--- list of comments/list-of-comments/src/index.js
+++ list of comments/list-of-comments/src/index.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail/>
+            <CommentDetail
+                author="Sam"
+                timeAgo="Today at 2:00PM"
+                content="Nice blog post!"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Jane"
+                timeAgo="Today at 10:00AM"
+                content="I like the subject"
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail
+                author="Taylor"
+                timeAgo="Yesterday at 2:00PM"
+                content="I like your writing"
+                avatar={faker.image.avatar()}
+            />
         </div>);
 };
 
