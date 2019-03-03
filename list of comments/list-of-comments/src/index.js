e3111e0de08d978c4d077db462d650432a9017f4
Aleksandra Gonera <goneraleksandra@gmail.com>
2019-03-03 14:09:43 +0100

    added approval card

--- list of comments/list-of-comments/src/index.js
+++ list of comments/list-of-comments/src/index.js
@@ -2,28 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail
-                author="Sam"
-                timeAgo="Today at 2:00PM"
-                content="Nice blog post!"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Jane"
-                timeAgo="Today at 10:00AM"
-                content="I like the subject"
-                avatar={faker.image.avatar()}
-            />
-            <CommentDetail
-                author="Taylor"
-                timeAgo="Yesterday at 2:00PM"
-                content="I like your writing"
-                avatar={faker.image.avatar()}
-            />
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 2:00PM"
+                    content="Nice blog post!"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Today at 10:00AM"
+                    content="I like the subject"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Taylor"
+                    timeAgo="Yesterday at 2:00PM"
+                    content="I like your writing"
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+
         </div>);
 };
 
