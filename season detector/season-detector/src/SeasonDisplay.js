961937caa5e05ecd9d57cb0b515953dea1a62b40
Aleksandra Gonera <goneraleksandra@gmail.com>
2019-03-04 22:30:43 +0100

    season detector app completed

--- season detector/season-detector/src/SeasonDisplay.js
+++ season detector/season-detector/src/SeasonDisplay.js
@@ -1,8 +1,45 @@
+import './SeasonDisplay.css';
 import React from 'react';
 
-const SeasonDisplay = () => {
+
+const seasonConfig = {
+    summer: {
+        text: 'Let\'s hit the beach!',
+        iconName: 'sun'
+    },
+    winter: {
+        text: 'Brr, it\'s chilly!',
+        iconName: 'snowflake'
+    }
+};
+
+// determine which season is
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        // return lat > 0 ? 'summer' : 'winter';
+        if (lat > 0) {
+            return 'summer';
+        } else {
+            return 'winter';
+        }
+    } else {
+        // return lat > 0 ? 'winter' : 'summer';
+        if (lat > 0) {
+            return 'winter';
+        } else {
+            return 'summer';
+        }
+    }
+};
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const {text, iconName} = seasonConfig[season];
     return (
-        <div>season display</div>
+        <div className={`season-display ${season}`}>
+            <i className={`massive ${iconName} icon`}/>
+            <h1>{text}</h1>
+        </div>
     );
 };
 
--- season detector/season-detector/src/index.js
+++ season detector/season-detector/src/index.js
@@ -1,35 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Loader from './Loader';
 
 class App extends React.Component {
-    constructor(props) {
 
+    constructor(props) {
         // get the functionality from React.Component
         super(props);
         // initialize state
-        this.state = {lat: null, errorMessage: ''};
+        this.state = {lat: null, lon: null, errorMessage: ''};
+    }
 
+    // or instead of the constructor:
+    // == state = {lat: null, errorMessage: ''};
+
+    // lifecycle methods:
+    componentDidMount() {
         // get the location
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                // updating the state object
-                this.setState({lat: position.coords.latitude});
-            },
-            err => {
-                this.setState({errorMessage: err.message});
-            }
+            // updating the state object
+            position => this.setState(
+                {
+                    lat: position.coords.latitude,
+                    lon: position.coords.longitude
+                }),
+            // handle errors
+            err => this.setState({errorMessage: err.message})
         );
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('Component was updated - it rerendered.');
+    }
+
+    // separate conditionals from render()
+    // we want only one return statement in render()
+    renderContent() {
+        // we just return some JSX
+        if (this.state.errorMessage && !this.state.lat) {
+            return (
+                <div>
+                    Error: {this.state.errorMessage}
+                </div>
+            );
+        } else if (this.state.lat && !this.state.errorMessage) {
+            // take state and pass it as a prop to a child
+            return <SeasonDisplay
+                lat={this.state.lat}
+                lon={this.state.lon}
+            />
+        } else {
+            return <Loader
+                message="Please accept location request"
+            />
+        }
     }
 
     // obligatory to define a render method
     render() {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div> Error: {this.state.errorMessage}</div>
-        } else if (this.state.lat && !this.state.errorMessage) {
-            return <div> Latitude: {this.state.lat}</div>
-        } else {
-            return <div>Loading...</div>
-        }
+        return (
+            <div>
+                {this.renderContent()}
+            </div>
+        );
     }
 }
 
